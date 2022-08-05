9b5480d12e8bd4efcdaec39f275af7724ed1ad5b
LeeHsss <lii0730@icloud.com>
2022-08-05 16:12:24 +0900

    메모 앱 인터페이스 설계

---
+++ MyMemory/MyMemory/MemoData.swift
@@ -0,0 +1,16 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by LeeHsss on 2022/08/05.
+//
+
+import UIKit
+
+class MemoData {
+    var momoIdx: Int?
+    var title: String?
+    var contents: String?
+    var image: UIImage?
+    var regDate: Date?
+}
